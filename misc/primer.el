9f5422c1606694841ef00e96456e83c8110f861a
Phil Armitage <philip.armitage@gmail.com>
2011-09-05 23:28:13 +0100

    start of new emacs mode

--- misc/primer.el
+++ misc/primer.el
@@ -2,25 +2,38 @@
 ;;; A simple emacs major mode for editing Primer code ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'generic-x)
+(defvar primer-mode-hook nil)
 
-(define-generic-mode 'primer-mode
-  '("#")
-  '("fn" "end" "if" "then" "else" "where" "is" "at" "as"
-    "and" "or" "not" "mod" "true" "false"
-    "int" "float" "char" "bool" "string" "list" "lambda")
-  '(("=" . 'font-lock-operator)
-    ("!" . 'font-lock-operator)
-    (":" . 'font-lock-operator)
-    ("+" . 'font-lock-operator)
-    ("-" . 'font-lock-operator)
-    ("*" . 'font-lock-operator)
-    ("/" . 'font-lock-operator)
-    ("<" . 'font-lock-operator)
-    (">" . 'font-lock-operator)
-    ("[A-Za-z][A-Za-z0-9]*" . 'font-lock-variable-name-face))
-  '(".pri\\'")
-  nil
-  "Major mode for editing Primer code.")
+(defvar primer-mode-map
+  (let ((map (make-keymap)))
+    (define-key map "\C-j" 'newline-and-indent)
+    map))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.pri\\'" . primer-mode))
+
+(defconst primer-font-lock-keywords
+  (list
+   '("^#.*"
+     . font-lock-comment-face)
+   '("\\<\\(else\\|if\\|let\\|then\\|using\\|\\|pragma\\|val\\|fun\\|in\\|match\\)\\>"
+     . font-lock-keyword-face)
+   '("\\(!=\\|\\+\\+\\|->\\|\\.\\.\\|<=\\|>=\\\|[&*+/<=>|~^-]\\)"
+     . font-lock-builtin-face)
+   '("\\<\\(and\\|mod\\|at\\|as\\|not\\|or\\)\\>"
+     . font-lock-builtin-face)
+   '("\\<\\(true\\|false\\|int\\|float\\|char\\|bool\\|string\\)\\>" . font-lock-constant-face)))
+
+(defun primer-mode ()
+  "Emacs major mode for editing Primer programs"
+  (interactive)
+  (kill-all-local-variables)
+  ;(set-syntax-table primer-mode-syntax-table)
+  (use-local-map primer-mode-map)
+  (set (make-local-variable 'font-lock-defaults) '(primer-font-lock-keywords))
+  ;(set (make-local-variable 'indent-line-function) 'primer-indent-line)
+  (setq major-mode 'primer-mode)
+  (setq mode-name "Primer")
+  (run-hooks 'primer-mode-hook))
 
 (provide 'primer-mode)
